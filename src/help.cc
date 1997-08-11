64320e0ea942614adce646ee00c66359034983e0
lav <>
1997-08-11 13:44:35 +0000

    *** empty log message ***

--- src/help.cc
+++ src/help.cc
@@ -1,6 +1,6 @@
-/* 
+/*
  * Copyright (c) 1993-1997 by Alexander V. Lukyanov (lav@yars.free.net)
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
@@ -16,6 +16,7 @@
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
+#include <config.h>
 #include <ctype.h>
 #include "edit.h"
 #include "keymap.h"
--- src/keymapdf.cc
+++ src/keymapdf.cc
@@ -1,6 +1,6 @@
-/* 
+/*
  * Copyright (c) 1993-1997 by Alexander V. Lukyanov (lav@yars.free.net)
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
@@ -24,6 +24,7 @@
 **_________________________________________________________________________
 */
 
+#include <config.h>
 #include <stdio.h>
 #include "keymap.h"
 
