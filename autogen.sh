c22fa483a74ae79c1ed50e3e600bbc540c70589c
lav <>
2004-12-30 11:21:49 +0000

    add -I m4

--- autogen.sh
+++ autogen.sh
@@ -102,7 +102,7 @@ do
     echo processing $dr
     macrodirs=`sed -n -e 's,AM_ACLOCAL_INCLUDE(\(.*\)),\1,gp' < $coin`
     ( cd $dr
-      aclocalinclude="$ACLOCAL_FLAGS"
+      aclocalinclude="$ACLOCAL_FLAGS -I m4"
       for k in $macrodirs; do
   	if test -d $k; then
           aclocalinclude="$aclocalinclude -I $k"
