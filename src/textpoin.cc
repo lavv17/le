70a2d21da2e9468c455d81c7a075334f23fdc905
lav <>
2013-01-31 08:09:29 +0000

    new copyright year

--- src/textpoin.cc
+++ src/textpoin.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1993-2012 by Alexander V. Lukyanov (lav@yars.free.net)
+ * Copyright (c) 1993-2013 by Alexander V. Lukyanov (lav@yars.free.net)
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
